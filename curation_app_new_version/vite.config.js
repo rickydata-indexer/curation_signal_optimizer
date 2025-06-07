de9a6cf65e089c23d8756cfcf9610f6ac3ec697a
rickydata-indexer <ricky@cambrian.org>
2025-06-07 12:34:47 +0100

    deploy to gitbub pages

--- curation_app_new_version/vite.config.js
+++ curation_app_new_version/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/curation-signal-optimizer/', // Replace with your GitHub repo name
   server: {
     port: 5174,
     allowedHosts: true
